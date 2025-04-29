d61b93d4d21e87545664ae8ee5575cd83bdcac42
Oscar Mier <voscarmv@gmail.com>
2025-04-29 19:18:16 +0000

    Configured solid channel for photo taken notifications

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+    # For action cable
+    Rails.application.routes.draw do
+      mount ActionCable.server => '/cable'
+    end
 end
